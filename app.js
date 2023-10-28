5aa9d5bc5f0ff340d6148c48da1f1564aa6b42e0
aribakha <140427904+aribakha@users.noreply.github.com>
2023-10-28 15:32:40 +0500

    Add files via upload
    js assignment

---
+++ app.js
@@ -0,0 +1,91 @@
+                          //  CHAPTER 3 2 "VARIABLE FOR STRING"
+                                    //  QUESTION # 1
+
+// var userName ="areeba"
+
+                                    //  QUESTION # 2
+                                  
+// var myName = "areebanasir"
+   
+                                    //  QUESTION # 3
+                                   
+// var message = "Hello World";
+// alert(message);
+
+
+                                    //  QUESTION # 4
+//  declare variables  for student's bio data  
+// var name = "jhone Doe";
+// var age = 15;
+// var certificate = "Certified Mobile Application Development";
+// // create a message using the variables
+// // var message = "Name:"+ name +"\nAge:"+ age +"years 0ld" +"\nCertified in:"+ certificate;
+// // Display the bio data in alert box  
+// alert(name);
+// alert(+ age+"years old");
+// alert(certificate)
+
+                                    // QUESTION # 5
+
+
+// var name = "pizza\npizz\npiz\npi\np"
+// alert(name)                            
+                                    // QUESTION # 6
+
+//  var email = " areebakhan@gmail.com";
+// alert("My email adress is" + email);                             
+
+                                    // QUESTION # 7
+
+// var book = "I am trying to learn from the book A smarter \n way to learn JavaScript"
+// alert(book)
+
+                                    // QUESTION # 8
+
+// var message = "Yah! I can write HTML content through JavaScript"
+// document.write(message)
+
+                                    // QUESTION # 9
+
+// var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(design)
+
+                                // CHAPTER # 3 "VARIAVLE FOR NUMBER"
+                                        // QUESTION # 1
+
+// var age = 15 
+// alert("I am "  + age + " years old ")
+
+                                        // QUESTION # 2
+
+// for(let i = 0; i = 1 ;i++){
+
+// }
+
+                                        // QUESTION # 3
+
+// var birthYear = 1990
+// document.write("My birth year is " + birthYear + "<br>" +"Data type of my declared variable is number") 
+
+                                        // QUESTION # 4
+
+// var visitorsName = "Jhone Doe"
+// var quantity = 1020
+// var produuctTitle = "famous clothing shop"
+
+// document.write(visitorsName+  " ordered " +  quantity + " shirts(L) on " + produuctTitle)
+
+ 
+
+                            // CHAPTER # 4 "VARIABLE NAMES : LEGAL & ILLEGAL"
+                                        // QUESTION 1
+                                        // PART A
+// document.write( "<h1>Rules for naming JS variables</h1>" )
+// document.write(" (a) variable names can only contain <u> numbers,$,_ </u>" +"</br>")
+// document.write(" (b) variable must begin with a <u>letters,$,_</u>" +"</br>")
+// document.write(" (c) variable names are case <u>sensitive</u>" +"</br>")
+// document.write(" (d) variable names should not be JS <u>keywords</u>" +"</br>")
+
+
+
+ 
